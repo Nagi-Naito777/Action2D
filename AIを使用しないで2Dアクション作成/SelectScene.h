01b894372d7cc72f5943057744195b7c8e3fb4a1
Nagi-Naito777 <19670111kenken@gmail.com>
2026-09-07 22:00:01 +0900

    テキストファイルを自動でリリースフォルダに生成するように設定完了

--- AIを使用しないで2Dアクション作成/SelectScene.h
+++ AIを使用しないで2Dアクション作成/SelectScene.h
@@ -8,4 +8,14 @@ public:
     SceneName Update() override;
 
     void Draw() const override;
+
+private:
+    // ステージ選択カーソル構造体
+    struct Cursol {
+        int X = 0;
+        int Y = 0;
+        const int X_Lim = 3;    // X座標の限界地
+        const int Y_Lim = 2;    // Y座標の限界地
+    };
+    Cursol Cur;
 };
